836579b411cb3b415ad912c47a0b1772e28bb8e8
Miihajlo <tosic.mihajlo08@gmail.com>
2022-07-07 19:37:40 +0200

    practice scope of a variable

---
+++ practice.js
@@ -0,0 +1,16 @@
+console.log("Hello world");
+let x=2;
+for(let i=0; i<=10; i++){
+    console.log(i);
+    x=10;
+    if(i===10){
+        console.log(x);     
+    }
+}
+/*for(i=0; i<=20; i++){
+    if(i%2==0)
+    {
+        console.log(i);
+    }
+    else console.log("greska");
+}*/
